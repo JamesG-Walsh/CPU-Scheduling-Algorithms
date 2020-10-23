b3715874e6a2ace8acd0ea05cc0cfab437f24408
James Walsh <jamesgcwalsh@gmail.com>
2020-10-23 13:15:01 -0400

    program compiles
    algorithm looks solid, but getting segmentation fault before entering rr_result

--- skeleton/scheduling.c
+++ skeleton/scheduling.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdbool.h> //added this myself //TODO ask if this is ok?
+#include <stdio.h> //same here
 
 #include "scheduling.h"
 
@@ -22,11 +24,72 @@
 */
 rr_result *rr(int *queue, int np, int tq)
 {
+	printf("Starting rr method...\n");
 	rr_result *result = malloc(sizeof(rr_result));
 	result->np = np;
 	result->turnarounds = malloc(sizeof(int) * np);
 
 	// code here to assign values to result->turnarounds, result->order, and result->order_n
 
+	int time_remaining[np]; //maybe just use queue instead? //or store here for checking against result?
+	int elapsed_time = 0;
+
+	int start_times[np]; //for calculating turnaround time.  start_times[i] is when pi started processing for the first time.
+	for(int i = 0; i < np; i++)
+	{
+		start_times[i] = -1; //initialize every value of start time to -1 to show that it has not been set yet.
+	}
+
+	result->order_n = 0;
+	//result->turnarounds = int[np];
+
+	bool queue_finished_processing = false;
+	printf("Starting Loop...\n");
+	while(!queue_finished_processing)
+	{
+		for(int i = 0; i < np ; i++) //iterate through queue of burst times
+		{
+			if(queue[i] == 0) //process already completed
+			{
+				printf("Skipping p%d.\n", (i+1));				//skip
+			}
+			else if (0 < queue[i] && queue[i] <= tq) //process completes this round
+			{
+				if(start_times[i] == -1) //if start time hasn't been set already
+				{
+					start_times[i] = elapsed_time; //set start time to current elapsed_time
+				}
+
+				elapsed_time += queue[i];
+				queue[i] = 0;
+
+				result->order[result->order_n] = i;
+				result->order_n += 1;
+
+				result->turnarounds[i] = (elapsed_time - start_times[i]);
+			}
+			else if (queue[i] > tq) //process will not complete this round
+			{
+				if(start_times[i] == -1) //if start time hasn't been set already
+				{
+					start_times[i] = elapsed_time; //set start time to current elapsed_time
+				}
+
+				elapsed_time += tq;
+				queue[i] -= tq;
+
+				result->order_n += 1;
+				result->order[result->order_n] = i;
+			}
+		}
+		queue_finished_processing = true; //while loop will end unless a process with time remaining is found.
+		for (int j = 0; j < np ; j++)// iterate through queue
+		{
+			if(queue[j] > 0)//if any process has time remaining
+			{
+				queue_finished_processing = false; //the while loop continues and round robin does another round.
+			}
+		}
+	}
 	return result;
 }
--- skeleton/types.h
+++ skeleton/types.h
@@ -11,10 +11,10 @@ typedef struct input_queues
 
 typedef struct rr_result
 {
-	int np;
-	int order_n;
-	int *order;
-	int *turnarounds;
+	int np; //number of processes
+	int order_n; // size of the order array
+	int *order; // pointer to array containing the execution order of processes
+	int *turnarounds; //pointer to array containing the turnaround time for each process.  turnarounds[0] = turnaround time of p1
 } rr_result;
 
 typedef struct node
